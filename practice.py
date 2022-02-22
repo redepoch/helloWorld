c1105f36c101e1500effd5bcbe99ff1e18a9c441
redEpoch <redepoch@paran.com>
2022-02-22 18:24:19 +0900

    first commit

---
+++ practice.py
@@ -0,0 +1,69 @@
+# print(3.14)
+# print(3*3+1)
+# print('풍선'*10)
+# print("나비"*9)
+# print(5 > 10)
+# print(True)
+# print(not True)
+
+# name = "연탄이"
+# animal = "강아지"
+# age = 4
+# print(animal + name + str(age))
+# print(animal + name, age)
+
+print(1+1)
+print(123%9)
+print(123/9)
+print(123//9)
+print(123 != 9)
+
+print(abs(-10))
+print(pow(4, 2))
+print(max(4, 2, 5))
+
+from math import *
+print(floor(4.88))
+print(ceil(3.14))
+
+from random import *
+print(random())
+print(int(random() * 10))
+print(int(random() * 10) + 1)
+print(randrange(1, 46))
+print(randint(1, 45))
+
+sentence = '나는 소년입니다.'
+sentence2 = "파이썬은 쉬워요"
+sentence3 = """
+나는 소년이고, 파이썬은 쉬워요.
+"""
+
+print(sentence[1:5])
+print(sentence2[2])
+print(sentence3)
+
+python = "Python is Amazing"
+print(python.lower())
+print(python.upper())
+print(len(python))
+
+print("나는 %s색과 %s색을 좋아해요." %("파란", "빨간"))
+print("나는 {}색과 {}색을 좋아해요." .format("파란", "빨간"))
+print("나는 {1}색과 {0}색을 좋아해요." .format("파란", "빨간"))
+
+age = 20
+color = "빨간"
+print(f"나는 {age}살이며, {color}색을 좋아해요.")
+
+print("가나다라마바사는 \n한글이다.")
+print("'가나다라마바사'는 한글이다.")
+# print("\'가나다라마바사\'는 한글이다.")
+print('"가나다라마바사"는 한글이다.')
+print("\"가나다라마바사\"는 한글이다.")
+
+subway = [10, 20, 30]
+print(subway[0])
+
+
+
